f78f74b1523d72f9e8cd65392e6931ebdcffa75b
makiper <michael.kiper@jpl.nasa.gov>
2025-09-29 16:31:28 -0700

    changed the 1nm resampling to resampling to emit wavelengths

--- src/cover_class/static/preprocessor.py
+++ src/cover_class/static/preprocessor.py
@@ -4,10 +4,11 @@ import torch
 from numpy.typing import NDArray
 import numpy as np
 from scipy.interpolate import interp1d # type: ignore[import]
+from isofit.core.common import resample_spectrum # type: ignore[import]
 
 def interior_interpolation(
         data_matrix: NDArray[np.float32], 
-        current_wavelengths:NDArray[np.float32],
+        current_wavelengths: NDArray[np.float32],
         ) -> Tuple[FloatTensor, Tensor]:
     wl_min = np.ceil(current_wavelengths.min())
     wl_max = np.floor(current_wavelengths.max())
@@ -17,5 +18,14 @@ def interior_interpolation(
     interpolated_spectra = f(target_wavelengths)
     return FloatTensor(torch.from_numpy(interpolated_spectra).to(torch.float32)), torch.from_numpy(target_wavelengths)
 
-def convolve(data_matrix:FloatTensor) -> FloatTensor: ... # type: ignore 
+def resample_emit_wls(
+    data_matrix: NDArray[np.float32], 
+    current_wavelengths: NDArray[np.float32],
+    target_wavelengths: NDArray[np.float32],
+    fwhm: NDArray[np.float32],
+) -> Tuple[FloatTensor, Tensor]:
+    s: NDArray[np.float32] = resample_spectrum(data_matrix, current_wavelengths, target_wavelengths, fwhm)
+    return FloatTensor(torch.from_numpy(s).to(torch.float32)), torch.from_numpy(target_wavelengths)
+
+def convolve(data_matrix: FloatTensor) -> FloatTensor: ... # type: ignore 
 
